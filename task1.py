215866dfb94e6490b7cab1a068adf76a004a6625
klaus <70817966+klaus-wq@users.noreply.github.com>
2025-06-16 13:37:50 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)  # Строковое поле с ограничением в 200 символов
-    body = models.TextField(null=True, blank=True)  # Необязательное текстовое поле
-    published = models.BooleanField(default=False)  # Булево поле со значением по умолчанию False
-    created_at = models.DateTimeField(auto_now_add=True)  # Автоматическое заполнение даты создания
-    updated_at = models.DateTimeField(auto_now=True)  # Автоматическое обновление даты при изменении
+    title = models.CharField(max_length=200) 
+    body = models.TextField(null=True, blank=True)
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
